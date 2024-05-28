7437ddf5ce2e3a2c458eaeacd4f1fd0d284b7fb8
Sunil8777 <suniljoshi8777@gmail.com>
2024-05-28 21:38:19 +0530

    third commit

--- src/screens/SeatBookingScreen.tsx
+++ src/screens/SeatBookingScreen.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 
+const timeArray:string[]=[
+  "10:30",
+  "12:30",
+  "14:30",
+  "15:00",
+  "19:30",
+  "21:00"
+];
+
+const generateDate =()=>{
+  const date =new Date();
+  let weekday =['Sun','Mon','Tue','Wed','Thu','Fri','Sat']
+  let weekdays = [];
+  for(let i=0;i<7;i++){
+    let tempDate ={
+      date:new Date(date.getTime()+ i *24*60*60*1000).getDate(),
+      day:weekday[new Date(date.getTime()+ i *24*60*60*1000).getDay()],
+    };
+    weekdays.push(tempDate);
+  }
+  console.log(weekdays)
+};
+
 const SeatBookingScreen = () => {
   return (
+    generateDate(),
     <View style={styles.container}>
       <Text>HomeScreen</Text>
     </View>
